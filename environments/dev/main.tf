0a06f8ebcfe99109a3a4ac20122abd278940ef22
shanilevy <shani.levy77@gmail.com>
2023-04-30 18:12:50 +1000

    test

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -203,7 +203,7 @@ resource "google_eventarc_trigger" "trigger-pubsub-tf" {
             region = var.region
         }
     }
-    transport_topic = = "projects/${var.project_id}/topics/gcs-new-file"
+    transport_topic = "projects/${var.project_id}/topics/gcs-new-file"
 }
 
 # resource "google_cloud_run_service_iam_member" "allUsers" {
